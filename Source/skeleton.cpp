b9fc75d84907096867f14e349c7a5e76de8a3f22
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-04-16 15:45:31 +0100

    small improvement

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -8,6 +8,7 @@ No recorded activity before January 30, 2017
 #include <SDL/SDL.h>
 #include "SDLauxiliary.h"
 #include "TestModel.h"
+#include "limits.h"
 
 using namespace std;
 using glm::vec3;
@@ -16,8 +17,8 @@ using glm::mat3;
 /* ----------------------------------------------------------------------------*/
 /* GLOBAL VARIABLES                                                            */
 
-const int SCREEN_WIDTH = 111;
-const int SCREEN_HEIGHT = 111;
+const int SCREEN_WIDTH = 200;
+const int SCREEN_HEIGHT = 200;
 SDL_Surface* screen;
 int t;
 // vector<vec3> stars(1000);
@@ -82,18 +83,23 @@ void Draw()
 
     LoadTestModel(triangles);
     Intersection intersection;
+    vec3 d;
     for (int i = 0; i < SCREEN_HEIGHT; i++)
     {
         for (int j = 0; j < SCREEN_WIDTH; j++)
         {
-            vec3 d(j - SCREEN_WIDTH / 2, i - SCREEN_HEIGHT / 2, f);
+            //vec3 d(j - SCREEN_WIDTH / 2, i - SCREEN_HEIGHT / 2, f);
+            d = vec3(-0.5 + j * 1 / SCREEN_WIDTH, -0.5 + i * 1 / SCREEN_HEIGHT, f);
             // for (int num; num < triangles.size(); num++)
             // {
             //     intersection_point(triangles[num], d, s);
             // }
             ClosestIntersection(s, d, triangles, intersection);
-            vec3 color( 1.0, 0.0, 0.0 );
+            //vec3 color( 1.0, 0.0, 0.0 );
             PutPixelSDL( screen, j, i, triangles[intersection.triangleIndex].color);
+            // float m = std::numeric_limits<float>::max();
+            // printf("%f\n", m);
+
         }
     }
 
@@ -121,20 +127,24 @@ bool ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>& triangles
     bool flag = false;
     float min = 0.0;
     int triangleIndex;
+    vec3 v0, v1, v2, e1, e2, b, x;
+    mat3 A;
     for (size_t i = 0; i < triangles.size(); i++) {
-        vec3 v0 = triangles[i].v0;
-        vec3 v1 = triangles[i].v1;
-        vec3 v2 = triangles[i].v2;
+        //printf("b\n");
+        v0 = triangles[i].v0;
+        v1 = triangles[i].v1;
+        v2 = triangles[i].v2;
 
-        vec3 e1 = v1 - v0;
-        vec3 e2 = v2 - v0;
-        vec3 b = start - v0;
+        e1 = v1 - v0;
+        e2 = v2 - v0;
+        b = start - v0;
 
-        mat3 A(-dir, e1, e2);
-        vec3 x = glm::inverse(A) * b;
-        //printf("%f %f %f\n", x[0], x[1], x[2]);
+        A = mat3(-dir, e1, e2);
+        //printf("a\n");
+        x = glm::inverse(A) * b;
+        printf("%f %f %f\n", x[0], x[1], x[2]);
         if (x[1] > 0 && x[2] > 0 && (x[1] + x[2]) < 1 && x[0] > 0) {
-            printf("a");
+            printf("c\n");
             if (!flag) {
                 min = x[0];
                 triangleIndex = i;
